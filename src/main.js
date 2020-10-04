2360b305861aa4704c9614b63dc623f7ee730568
lane-c-wagner <lane.c.wagner@gmail.com>
2020-10-04 13:05:30 -0600

    tag manager

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Notifications from 'vue-notification';
+import VueGtm from 'vue-gtm';
 
 Vue.config.productionTip = false;
 
 Vue.use(Notifications);
+
+Vue.use(VueGtm, {
+  id: [
+    'GTM-PT7FHFB'
+  ],
+  defer: true,
+  debug: false,
+  loadScript: true,
+  trackOnNextTick: false
+});
 
 new Vue({
   store,
