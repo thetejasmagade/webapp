ea45d5ae166188b9dfb8d5f97fe73331e7b76af0
lane-c-wagner <lane.c.wagner@gmail.com>
2020-12-16 13:57:52 -0700

    modal background

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -66,7 +66,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(0, 0, 0, 0.95);
   display: flex;
   flex-direction: column;
   justify-content: center;
