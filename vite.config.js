9ed948fa91f5042a5116ef0f1a02e44a78b8b0d6
lane-c-wagner <lane.c.wagner@gmail.com>
2021-04-22 11:17:14 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import {createVuePlugin} from 'vite-plugin-vue2';
+import { createVuePlugin } from 'vite-plugin-vue2';
 import path from 'path';
 
 export default defineConfig({
@@ -16,6 +16,7 @@ export default defineConfig({
     ]
   },
   build: {
-    chunkSizeWarningLimit: 600
+    chunkSizeWarningLimit: 600,
+    cssCodeSplit: false
   }
 });
