e8f7eb8acf00cd50fc9bef743b87653d387e412d
wagslane <lane.c.wagner@gmail.com>
2021-11-20 08:49:30 -0700

    api url

--- src/lib/cloudClient.js
+++ src/lib/cloudClient.js
@@ -5,7 +5,7 @@ import {
   removeCloudJWT
 } from './localStorageLib';
 
-export const domain = 'https://qvault-classroom.wm.r.appspot.com';
+export const domain = 'https://api.qvault.io';
 
 export function loginToken(token) {
   saveCloudJWT(token);
