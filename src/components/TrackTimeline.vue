d4740153fa062421e22e1adcc5543619d0a8e7e3
Allan Lires <allanlires95@gmail.com>
2021-12-01 21:18:23 -0700

    WIP Skeleton Loader

--- src/components/TrackTimeline.vue
+++ src/components/TrackTimeline.vue
@@ -2,39 +2,44 @@
   <div class="relative wrap overflow-hidden p-10 h-full">
     <div
       class="
-            border-2-2 absolute border-opacity-20 border-gray-700 h-full border
-            hidden lg:block
-        "
+        border-2-2
+        absolute
+        border-opacity-20 border-gray-700
+        h-full
+        border
+        hidden
+        lg:block
+      "
       style="left: 50%"
     />
-    <div
-      v-for="(unit, i) of units"
-      :key="i"
-    >
+    <div v-for="(unit, i) of units" :key="i">
       <div
         class="flex justify-center items-center w-full left-timeline"
         :class="{
-          'left-timeline': i%2 === 0,
-          'right-timeline': i%2 !==0,
-          'flex-row-reverse': i%2 === 0,
-          'lg:-mt-48': i > 0
+          'left-timeline': i % 2 === 0,
+          'right-timeline': i % 2 !== 0,
+          'flex-row-reverse': i % 2 === 0,
+          'lg:-mt-48': i > 0,
         }"
       >
+        <div class="order-1 w-1/3 hidden lg:block" />
         <div
           class="
-            order-1 w-1/3
-            hidden lg:block
-        "
-        />
-        <div
-          class="
-                z-20 items-center order-1 shadow-xl w-12 h-12 rounded-full
-                hidden lg:flex
-            "
-          :class="{'bg-gray-400': i > 0, 'bg-blue-500': i === 0}"
+            z-20
+            items-center
+            order-1
+            shadow-xl
+            w-12
+            h-12
+            rounded-full
+            hidden
+            lg:flex
+          "
+          :class="{ 'bg-gray-400': i > 0, 'bg-blue-500': i === 0 }"
         >
           <h1 class="mx-auto text-white font-semibold text-md">
-            {{ i%2 === 0 ? '←' : null }} {{ i + 1 }} {{ i%2 !== 0 ? '→' : null }}
+            {{ i % 2 === 0 ? "←" : null }} {{ i + 1 }}
+            {{ i % 2 !== 0 ? "→" : null }}
           </h1>
         </div>
         <div class="order-1 lg:w-1/3 items-center">
@@ -43,9 +48,13 @@
             :click="() => clickCallback(unit)"
             class="lg:mx-8"
           >
-            <UnitCardBody
-              :unit="unit"
-            />
+          <skeleton-loader-vue
+            type="circle"
+            :width="200"
+            :height="200"
+            animation="fade"
+              />
+            <UnitCardBody :unit="unit" />
           </ImageCard>
         </div>
       </div>
@@ -54,32 +63,33 @@
 </template>
 
 <script>
-import ImageCard from '@/components/ImageCard.vue';
-import UnitCardBody from '@/components/UnitCardBody.vue';
-import { getUnitData } from '@/lib/unit.js';
+import ImageCard from "@/components/ImageCard.vue";
+import UnitCardBody from "@/components/UnitCardBody.vue";
+import { getUnitData } from "@/lib/unit.js";
+import VueSkeletonLoader from "skeleton-loader-vue";
 
 export default {
   components: {
     ImageCard,
-    UnitCardBody
+    UnitCardBody,
+    VueSkeletonLoader
   },
   props: {
     clickCallback: {
       type: Function,
       required: false,
-      default: () => {}
+      default: () => {},
     },
     units: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
-    getUnitData
-  }
+    getUnitData,
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
