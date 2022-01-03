3efefa79ce03217a4cb4890298188c9c37cc77db
allanlires95@gmail.comalires <allanlires95@gmail.com>
2022-01-03 00:07:24 -0700

    reproduced issue with fake data

--- src/components/TrackTimeline.vue
+++ src/components/TrackTimeline.vue
@@ -59,7 +59,7 @@
     <div v-else>
       <div
       
-        v-for="(unit, i) of units"
+        v-for="(unit, i) of fakeData"
         :key="i"
       >
         <div
@@ -168,13 +168,27 @@ export default {
     },
     firstIncompleteIndex() {
       for (let i = 0; i < this.units.length; i++) {
-        if (!this.units[i].course.CompletedAt) {
-          return i;
-        }  
-        else if(!this.units[i].project.CompletedAt)
-          return i;
+        if (this.units[i].type === 'course'){
+          if (!this.units[i].course.CompletedAt) {
+            return i;
+          }  
+        }
+        else if (this.units[i].type === 'project')
+          if (!this.units[i].project.CompletedAt) {
+            return i;
+          } 
       }
       return null;
+    },
+    fakeData() {
+      let fakeData = [];
+      for (let i = 0; i < this.units.length; i++) {
+        fakeData[i] = this.units[i];
+        if (fakeData[i].type === 'course') {
+          fakeData[i].course.CompletedAt = '123';
+        }
+      }
+      return fakeData;
     }
   },
   methods: {
