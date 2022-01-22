d53dcf875a80536ce332a1aa497e25f69448fdf6
Allan Lires <AllanLires95@gmail.com>
2022-01-21 18:39:29 -0700

    Agnostic progress bar added. Does not render when not logged in

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="w-full bg-gray-200 h-full">
+      <div
+        class="bg-gold-400 h-2"
+        :style="setProgress"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    progressFilled: {
+      type: Number,
+      required: false,
+      default: null
+    }
+  },
+  computed: {
+    setProgress() {
+      let amountFilled = `width: ${this.progressFilled}%`;
+      return amountFilled;
+    }
+  }
+};
+
+</script>
