d64ad036c1d1eba4a6d3fdee5b2c387766a6326b
wagslane <lane.c.wagner@gmail.com>
2022-02-22 20:57:20 -0700

    value

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -205,7 +205,6 @@ export function eventEarnGems(numGems) {
     window.dataLayer.push({
       event: "earn_virtual_currency",
       virtual_currency_name: "Gems",
-      value: numGems,
     });
     mixpanel.track("earnVirtualCurrency", {
       numGems,
