ceebb257fa111197f821bea4d82ed64f444ea792
Allan Lires <AllanLires95@gmail.com>
2022-05-28 14:58:24 -0600

    more fixes

--- src/lib/platform.js
+++ src/lib/platform.js
@@ -3,9 +3,9 @@ export const WINDOWS = "WINDOWS";
 export const LINUX = "LINUX";
 export const DEFAULT = "NOTFOUND";
 export function getOperatingSystem() {
-  let platform = navigator.userAgentData.platform.toLowerCase();
-  if (platform.includes("mac")) return MAC;
-  else if (platform.includes("windows")) return WINDOWS;
-  else if (platform.includes("linux")) return LINUX;
+  let platform = navigator?.userAgent?.toLowerCase();
+  if (platform?.includes("mac")) return MAC;
+  else if (platform?.includes("windows")) return WINDOWS;
+  else if (platform?.includes("linux")) return LINUX;
   else return DEFAULT;
 }
