ca62fd54cb8aae55ce1a3346cf21252f8ed3a0a3
wagslane <lane.c.wagner@gmail.com>
2022-05-29 16:59:52 -0600

    fix

--- src/lib/cloudClient.js
+++ src/lib/cloudClient.js
@@ -1,7 +1,7 @@
 import decodeJWT from "./decodeJWT";
 import { saveCloudJWT, loadCloudJWT, removeCloudJWT } from "./localStorageLib";
 
-export const domain = "http://localhost:5000";
+export const domain = "https://api.qvault.io";
 
 export function loginToken(token) {
   saveCloudJWT(token);
