15a885519a88b77558424941ade598b0aa2b14ba
allanlires95@gmail.comalires <allanlires95@gmail.com>
2022-06-23 10:37:08 -0600

    fixed hint tab on bottom markdown

---
+++ src/components/radials/ProgressRadial.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Radial
+      :fill="percent"
+      :radius="24"
+      :max-fill="100"
+      :stroke-width="5"
+      :checkmark="isComplete"
+    />
+  </div>
+</template>
+
+<script>
+import Radial from "@/components/radials/Radial.vue";
+
+export default {
+  components: {
+    Radial,
+  },
+  props: {
+    percent: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    isComplete() {
+      const maxPercent = 100;
+      if (this.percent >= maxPercent) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
